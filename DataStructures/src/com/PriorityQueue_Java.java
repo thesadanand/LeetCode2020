18d54ee82acc6dd455dcb8ec529d9d6a5e7f2027
sadanandm <saddanand@gmail.com>
2020-11-06 23:15:00 +0530

    dynamic programming basic

--- DataStructures/src/com/PriorityQueue_Java.java
+++ DataStructures/src/com/PriorityQueue_Java.java
@@ -1,10 +1,13 @@
 package com;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.PriorityQueue;
 
 public class PriorityQueue_Java {
 
     public static void main(String[] args) {
+//        Queue<Integer> q2 = new LinkedList<>();
         PriorityQueue<Integer> q = new PriorityQueue<>();
         q.add(12);
         q.add(9);
